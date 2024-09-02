7b0d24e90484140755911f6eb7143c4b537f1ebd
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-09-02 18:47:05 +0545

    final commit

--- app.py
+++ app.py
@@ -122,5 +122,5 @@ def leave(data):
 
 # --------------------------Run the program------------------------
 if __name__ == '__main__':
-    # socketio.run(app)
-    app.run(debug=True)
+    socketio.run(app)
+    # app.run(debug=True)
